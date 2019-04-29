f779cf8f23856d53ccce026ebf8ecc595617a3d7
sivakarthik thathamsetty <thathamsettys@dnb.com>
2019-04-28 20:58:55 -0700

    New Python project

---
+++ venv/lib/python3.7/Trial.py
@@ -0,0 +1,21 @@
+
+
+
+class Sentence():
+    def __init__(self,sentence):
+        self.sentence=sentence
+        self.index=0
+        self.words=self.sentence.split()
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.index>=len(self.words):
+            raise StopIteration
+        idx=self.index
+        self.index+=1
+        return self.words[idx]
+
+st=Sentence("This is fun!!!!")
+
+for word in st:
+    print(word)
